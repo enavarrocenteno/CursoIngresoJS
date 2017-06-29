77f32cce9e7e9bd551fc0f9b8954b59817216f8f
enavarrocenteno <enavarrocenteno@gmail.com>
2017-06-28 21:58:34 -0300

    Add files via upload

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,29 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var dato=0;
+	//dato="octavio";
+//dato=document.getElementById('elNombre').value;
+//	alert(dato);
+document.getElementById('elNombre').value="natalianatalia"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
