54d88a71d52cb06b231e8e12f56faa76d9e28886
enavarrocenteno <enavarrocenteno@gmail.com>
2018-02-05 20:54:43 -0300

    ejercicio 1 y 2 (nuevos)
    Ver el alart, promt, variables y concatenar.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,23 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+ // esto es un comentario de una linea
+	//alert("anda");
+	//toda linea de instruccion termina con ;
+	var nombre;
+	/*
+	comentario en bloque
+	alert("nombre");
+	alert(nombre);
+	nombre="Ezequiel"
+	alert(nombre);
+	*/
+	//nombre= prompt ("ingrese su nombre","natalia natalia");
+	//alert(nombre);
+
+	var nombre=prompt("Cual es tu nombre");
+	alert("Bienvenido "+nombre);
 	
-}
+		//var person = prompt("Please enter your name", "Harry Potter");
+			}
 
